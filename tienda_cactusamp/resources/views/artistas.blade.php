2dc3ecaec5c77f5bc322b4783ff08814d317bb36
diego <diego@debian-BULLSEYE-live-builder-AMD64>
2024-01-31 15:58:30 -0300

    Mejora en el responsive

--- tienda_cactusamp/resources/views/artistas.blade.php
+++ tienda_cactusamp/resources/views/artistas.blade.php
@@ -1,10 +1,10 @@
 @include('/generico/header')
     <main>
-        <section class="primer_vista px-4">
+        <section class="primer_vista px-lg-4">
             <br><h4><i>Artistas que eligieron nuestros productos</i></h4>
             <div class="layout-responsive my-4">
                 @for ($i = 0; $i<$artistas; $i++)
-                    <div class="card">
+                    <div class="card artistas">
                         <img src="https://images.unsplash.com/photo-1603871165848-0aa92c869fa1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">Artista {{$i}}</h5>
--- tienda_cactusamp/resources/views/inicio.blade.php
+++ tienda_cactusamp/resources/views/inicio.blade.php
@@ -8,7 +8,7 @@
         <section class="faqs landing">
             <h5 class="my-4">PREGUNTAS FRECUENTES</h5>
             <div class="row justify-content-center">
-                <div class="col-6 text-center pt-5">
+                <div class="col col-lg-6 text-center pt-5">
                     <iframe width="560" height="315" src="https://www.youtube.com/embed/Nl3tuwvXxz4?si=SAeWTT2v70L10-Mt" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 </div>
             </div>
